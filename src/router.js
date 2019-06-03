ee23da8cc8ca9e4bdb3fee4f728eeca810e800dc
zw890907 <514970358@qq.com>
2019-06-03 23:11:07 +0800

    "chore:代码完善"

--- src/router.js
+++ src/router.js
@@ -4,19 +4,25 @@ import Home from './views/Home'
 import List from './views/List'
 import Category from './views/Category'
 import Test from './components/Test'
+import Login from './views/Login'
 
 Vue.use(Router)
 
+let isLogin = false
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/home',
+      alias: '/',
       name: 'home',
       component: Home
     },
     {
       path: '/list',
       name: 'list',
+      redirect: to => {
+        console.log(to)
+      },
       component: List,
       children: [
         {
@@ -28,6 +34,11 @@ export default new Router({
           }
         }
       ]
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
